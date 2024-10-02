d94880b303fdcc44a247d2516308553add503613
ProstoI7 <161821721+ProstoI7@users.noreply.github.com>
2024-10-02 12:46:34 +0700

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,54 +24,10 @@ namespace WpfApp4
         }
         private void ColorBrush_(object sender, SelectionChangedEventArgs e)
         {
-            if (ColorBrush.SelectedIndex == 0)
-            {
-                DrawingAttributes.Color = Colors.Black;
-            }
-            if (ColorBrush.SelectedIndex == 1)
-            {
-                DrawingAttributes.Color = Colors.Red;
-            }
-            if (ColorBrush.SelectedIndex == 2)
-            {
-                DrawingAttributes.Color = Colors.Green;
-            }
-            if (ColorBrush.SelectedIndex == 3)
-            {
-                DrawingAttributes.Color = Colors.Blue;
-            }
-            if (ColorBrush.SelectedIndex == 4)
-            {
-                DrawingAttributes.Color = Colors.Yellow;
-            }
-            if (ColorBrush.SelectedIndex == 5)
-            {
-                DrawingAttributes.Color = Colors.Orange;
-            }
-            if (ColorBrush.SelectedIndex == 6)
-            {
-                DrawingAttributes.Color = Colors.Purple;
-            }
-            if (ColorBrush.SelectedIndex == 7)
-            {
-                DrawingAttributes.Color = Colors.Pink;
-            }
-            if (ColorBrush.SelectedIndex == 8)
-            {
-                DrawingAttributes.Color = Colors.Gray;
-            }
-            if (ColorBrush.SelectedIndex == 9)
-            {
-                DrawingAttributes.Color = Colors.Brown;
-            }
-            if (ColorBrush.SelectedIndex == 10)
-            {
-                DrawingAttributes.Color = Colors.Turquoise;
-            }
-            if (ColorBrush.SelectedIndex == 11)
-            {
-                DrawingAttributes.Color = Colors.Lavender;
-            }
+          if (ColorBrush.SelectedItem != null)
+          {
+                DrawingAttributes.Color = (Color)ColorConverter.ConvertFromString((string)((ComboBoxItem)ColorBrush.SelectedItem).Tag);
+          }
         }
 
         private void BrushSize_(object sender, RoutedPropertyChangedEventArgs<double> e)
